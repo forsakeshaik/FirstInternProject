830303d37ebe435f829b16049b4dcc120cd94c3f
forsakeshaik <134041704+forsakeshaik@users.noreply.github.com>
2024-07-18 20:06:17 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+def listen(duration):
+	t= sr.Recognizer()
+	with sr.Microphone() as source:
+		text = t.record(source, duration=duration)
+		try:
+			return t.recognize_google(text)
+		except:
+			return "Didn't hear perfectly!"
